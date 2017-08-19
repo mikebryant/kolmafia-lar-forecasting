0bedccae506947d1ecbb93b2de0af5e3fcc6d183
Mike Bryant <mike@mikebryant.me.uk>
2017-08-19 14:03:40 +0100

    First draft of spreadsheet conversion

---
+++ util/convert.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import csv
+import re
+import sys
+
+def sanitise_location(data, loc):
+    loc = loc.lower()
+    loc = re.sub(" \(delay[\s\w]*\)", "", loc)
+
+    match = re.match("bathole \((.*)\)", loc)
+    if match is not None:
+        loc = match.groups()[0]
+
+    match = re.match("pyramid \((.*)\)", loc)
+    if match is not None:
+        loc = match.groups()[0]
+
+    loc = loc.replace("castle in the sky", "castle in the clouds in the sky")
+    loc = loc.replace("hey deze arena", "infernal rackets backstage")
+    loc = loc.replace("belilafs comedy club", "laugh floor")
+
+    if loc.lower() not in data and "the " + loc.lower() not in data:
+        print("Unknown location: " + loc)
+        return ""
+
+    return loc
+
+def convert(fobj):
+    csvreader = csv.reader(fobj)
+
+    with open("locations.txt") as locations_file:
+        locations_data = [line.strip().lower() for line in locations_file.readlines()]
+    with open("monsters.txt") as monsters_file:
+        monsters = [line.strip().lower() for line in monsters_file.readlines()]
+
+    locations = []
+    for row in csvreader:
+        if row[0] == 'Turn Count':
+            locations = [sanitise_location(locations_data, loc) for loc in row[1:]]
+        elif locations and row[0]:
+            for loc, enc in zip(locations, row[1:]):
+                if not enc or not loc:
+                    continue
+                if enc.lower() in monsters:
+                    combat = "true"
+                    encounter_name = ""
+                    monster_name = enc
+                else:
+                    combat = "false"
+                    encounter_name = enc
+                    monster_name = "none"
+                yield (
+                    loc, # Location
+                    row[0], # Turn number
+                    "0",# Delay
+                    combat,
+                    monster_name,
+                    encounter_name,
+                )
+
+def main(argv):
+    if len(argv) < 3:
+        print("""
+            Usage: ./convert.py expanded.csv lar_encounter_data_v1.txt
+
+            Download from https://docs.google.com/spreadsheets/d/1HQk1ANU-Uu4VrY5i7KDwPIrDQg37atFGIV1xra1nMfI/
+            Use `File` -> `Download as` -> `Comma-separated values`
+        """)
+        sys.exit(1)
+    else:
+        with open(argv[1]) as file_in:
+            with open(argv[2], "w") as file_out:
+                for line in convert(file_in):
+                    file_out.write("\t".join(line) + "\n")
+
+if __name__ == "__main__":
+    main(sys.argv)
